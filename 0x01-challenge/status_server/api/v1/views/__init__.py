7664b24e26163b6fcde2b15114c7995fb1922fb9
Amany Mohammed <amany.mohammd@gmail.com>
2024-07-22 04:32:47 +0300

    [fix] 0. Server status

--- 0x01-challenge/status_server/api/v1/views/__init__.py
+++ 0x01-challenge/status_server/api/v1/views/__init__.py
@@ -4,3 +4,7 @@
 from api.v1.views.index import *
 from flask import Blueprint
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
+
+app_views.add_url_rule(
+    "/status", methods=["GET"], view_func=status)  # << added
