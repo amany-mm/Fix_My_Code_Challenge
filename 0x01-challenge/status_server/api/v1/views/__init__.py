e3b500518ff225b291bb8c430ba5853cfcc7568a
Amany Mohammed <amany.mohammd@gmail.com>
2024-07-24 05:41:30 +0300

    Try to fix checker

--- 0x01-challenge/status_server/api/v1/views/__init__.py
+++ 0x01-challenge/status_server/api/v1/views/__init__.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """ Views module
 """
-from api.v1.views.index import status  # Updated
 from flask import Blueprint
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-
-app_views.add_url_rule(
-    "/status", methods=["GET"], view_func=status)  # << added
+from api.v1.views.index import status
+app_views.add_url_rule("/status", methods=["GET"], view_func=status)
